136e72f590a638fcb1b3fc92909354a69e944a75
serhiifilatov <zefippppp@gmail.com>
2024-10-01 09:50:44 +0300

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,44 @@
+plugins {
+    kotlin("multiplatform") version "1.9.0"
+    id("io.ktor.plugin") version "2.3.4"
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.0"
+    application
+}
+
+repositories {
+    mavenCentral()
+    maven { url = uri("https://maven.pkg.jetbrains.space/public/p/ktor/eap") }
+}
+
+kotlin {
+    val hostOs = System.getProperty("os.name")
+    val arch = System.getProperty("os.arch")
+    //jvm("native")
+    linuxX64("native") {
+        binaries {
+            executable {
+                entryPoint = "main"
+            }
+        }
+    }
+
+    sourceSets {
+        val nativeMain by getting {
+            dependencies {
+                implementation("io.ktor:ktor-server-core:2.3.4")
+                implementation("io.ktor:ktor-server-cio:2.3.4")
+            }
+        }
+        val nativeTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+                implementation("io.ktor:ktor-server-test-host:2.3.4")
+            }
+        }
+    }
+}
+
+/*
+application {
+    mainClass.set("com.example.MainKt") // Замініть "com.example.MainKt" на повний шлях до вашого головного класу
+}*/
